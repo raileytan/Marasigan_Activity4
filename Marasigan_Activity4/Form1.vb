12add9253784638eca8f60dae93b1fd1098771fc
STC <STC@DESKTOP-8DNJ21P>
2024-02-08 15:12:27 +0800

    Add project files.

---
+++ Marasigan_Activity4/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub btnDivision_Click(sender As Object, e As EventArgs) Handles btnDivision.Click
+        Dim a As Integer
+        Dim b As Integer
+
+        a = txtFirstnum.Text
+        b = txtSecondnum.Text
+
+        lblResult.Text = a / b
+
+
+    End Sub
+End Class
